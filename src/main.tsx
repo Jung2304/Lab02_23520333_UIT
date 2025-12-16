b2160528a47b1092b0503470e690fae86f074987
JungNguyen <minhdung38109141@gmail.com>
2025-12-16 19:43:39 +0700

    This is all lab02

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ const App = () => (
         marginBottom: "24px",
       }}
     >
-      Nguyễn Minh Dũng 23520333 - Lab05
+      Nguyễn Minh Dũng 23520333 - Lab02
     </h1>
 
     <Counter initialCount={0} />
